8a55016b4ee693a96eeb5b31750755b6e7d565ee
Miguel Angel Amengual <amengualma6@gmail.com>
2025-10-10 16:43:17 -0300

    Se arregla conflicto con el dom route

--- changoLite/src/App.jsx
+++ changoLite/src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import NavBar from './components/NavBar.jsx'
 import Home from './components/Home.jsx'
 import Footer from './components/footer';
+import {Route, Routes} from "react-router-dom";
+import Offers from "./components/offers.jsx";
 
 export default function App() {
     return (
         <>
             <NavBar/>
             <main className="container">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/ofertas" element={<Offers/>}/>
+                </Routes>
                 <Home/>
+
             </main>
             <Footer/>
         </>
