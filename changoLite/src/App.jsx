081de0756c8d7395bca5b98fda24d7c820a7ccff
Miguel Angel Amengual <amengualma6@gmail.com>
2025-10-11 14:08:42 -0300

    Se cambia el icono del carrito

--- changoLite/src/App.jsx
+++ changoLite/src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import NavBar from './components/NavBar.jsx'
-import Home from './components/Home.jsx'
 import Footer from './components/footer';
-import {Route, Routes} from "react-router-dom";
-import Offers from "./components/offers.jsx";
 import AppRoutes from "./routes/index.jsx";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function App() {
     return (
         <>
-
+            <link rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+            />
             <NavBar />
             <main className="container">
                 <AppRoutes />
