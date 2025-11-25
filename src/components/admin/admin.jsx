5d931b1505aad932aa1f00c50062471535676acc
Miguel Angel Amengual <amengualma6@gmail.com>
2025-11-25 09:38:31 -0300

    Se mejoran estilos de admin y se instala react icons

--- src/components/admin/admin.jsx
+++ src/components/admin/admin.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import { Outlet, NavLink } from 'react-router-dom'
 import { ROUTES } from "../../routes/paths.js"
+import { FaBoxOpen, FaUserShield, FaChartBar } from "react-icons/fa"
 
 export default function Admin() {
     return (
         <section className="admin">
-            <h1>Panel Admin</h1>
+            <h1>Panel de Administración</h1>
 
             <nav className="admin-nav">
                 <NavLink
                     to={ROUTES.adminProducts}
                     className={({ isActive }) => `admin-card ${isActive ? 'active' : ''}`}>
-                    🛒 Productos
+                    <FaBoxOpen size={28} style={{ marginBottom: '8px' }} />
+                    Productos
                 </NavLink>
 
                 <NavLink
                     to="users"
                     className={({ isActive }) => `admin-card ${isActive ? 'active' : ''}`}>
-                    👤 Usuarios
+                    <FaUserShield size={28} style={{ marginBottom: '8px' }} />
+                    Usuarios
                 </NavLink>
 
                 <NavLink
                     to="reports"
                     className={({ isActive }) => `admin-card ${isActive ? 'active' : ''}`}>
-                    📊 Reportes
+                    <FaChartBar size={28} style={{ marginBottom: '8px' }} />
+                    Reportes
                 </NavLink>
             </nav>
         </section>
